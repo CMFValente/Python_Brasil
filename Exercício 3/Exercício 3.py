d1ce171e68a99ac6790c3a4c75f8b1b2365abbad
Catarina Valente <catarina.mfv@gmail.com>
2021-09-17 10:33:45 +0100

    Exercise 3
    Sum fo Numbers

---
+++ Exercício 3/Exercício 3.py
@@ -0,0 +1,9 @@
+def sumNumbers ():
+    
+    number1 = input ('Enter your first number:')
+    number2 = input ('Enter your second number:')
+
+    soma = (float (number1) + float (number2))
+    print (soma)
+
+sumNumbers ()
